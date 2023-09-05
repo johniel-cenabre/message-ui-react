e134f401d5078f9df6db40ac4432c508b73556ed
johniel-cenabre <48662445+johniel-cenabre@users.noreply.github.com>
2023-09-05 22:01:35 +0800

    Release 2
    * Feature/chat store (#2)
    
    * Create chat store
    
    * Dispatch join chat after input user name
    
    * Dispatch send message and update chat
    
    * Chat history limit to 25 (#3)
    
    * Change system message style (#4)

--- src/components/chatroom/Chatroom.jsx
+++ src/components/chatroom/Chatroom.jsx
@@ -1,8 +1,33 @@
 import {Button, Card, Form, Input, Row, Space, Typography} from 'antd';
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import styles from './Chatroom.module.css';
+import {SEND_MESSAGE, UPDATE_CHAT, chatStore} from '../../store/chatStore';
+
+const PAGE_SIZE = 25;
 
 function Chatroom({username}) {
+
+  const [chatState, setChatState] = useState(chatStore.getState());
+  const [visibleChatCount, setVisibleChatCount] = useState(PAGE_SIZE);
+
+  useEffect(() => {
+    const storageListener = (e) => {
+      if (e.key === 'message-ui-react') {
+        chatStore.dispatch({type: UPDATE_CHAT, state: JSON.parse(e.newValue)});
+        setChatState(chatStore.getState());
+      }
+    };
+
+    window.addEventListener('storage', storageListener);
+
+    return () => window.removeEventListener('storage', storageListener);
+  }, []);
+
+  const visibleChat = useMemo(() => {
+    return chatState?.slice(-visibleChatCount);
+  }, [chatState, visibleChatCount]);
+
+  const hasMorePreviousChats = chatState.length !== visibleChat.length;
 
   const formRef = useRef();
   const inputRef = useRef();
@@ -10,73 +35,63 @@ function Chatroom({username}) {
 
   useEffect(() => {
     bottomRef.current.scrollIntoView();
-  });
+  }, [chatState]);
 
-  const onReset = async () => {
+  const onViewPreviousChats = useCallback(() => {
+    setVisibleChatCount((curr) => curr + PAGE_SIZE);
+  }, []);
+
+  const onReset = useCallback(async () => {
     formRef.current.resetFields();
-  };
+  }, [formRef]);
 
-  const onSend = async ({message}) => {
+  const onSend = useCallback(async ({message}) => {
     if (message === "") {
       return;
     }
 
+    chatStore.dispatch({
+      type: SEND_MESSAGE,
+      username,
+      message
+    });
+
     await onReset();
     inputRef.current.focus();
-  };
+  }, [inputRef]);
 
   return (
     <Form className={styles.chatroom} ref={formRef} onFinish={onSend}>
-      <Row className={styles.chatrow} justify="start">
-        <Card className={styles.messagebox}>
-          <Row>
-            <Typography.Text className={styles.user}>User 4</Typography.Text>
+      {hasMorePreviousChats && (
+        <Row justify="center">
+          <Button type="link" onClick={onViewPreviousChats}>see previous chats</Button>
+        </Row>
+      )}
+      {visibleChat?.map((chat, index) => {
+
+        const isSystemMessage = !chat.username;
+
+        if (isSystemMessage) {
+          return <Row key={(chat.message + '' + index)} className={styles.chatrow} justify="start">
+            <Typography.Text className={styles.system}>{chat.message}</Typography.Text>
+          </Row>;
+        }
+
+        const isCurrentUserMessage = chat.username === username;
+
+        return (
+          <Row key={(chat.message + '' + index)} className={styles.chatrow} justify={isCurrentUserMessage ? 'end' : 'start'}>
+            <Card className={`${styles.messagebox}${isCurrentUserMessage ? ` ${styles.you}` : ''}`}>
+              <Row>
+                <Typography.Text className={styles.user}>{isCurrentUserMessage ? 'You' : chat.username}</Typography.Text>
+              </Row>
+              <Row>
+                <Typography.Text className={styles.message}>{chat.message}</Typography.Text>
+              </Row>
+            </Card>
           </Row>
-          <Row>
-            <Typography.Text className={styles.message}>hi</Typography.Text>
-          </Row>
-        </Card>
-      </Row>
-      <Row className={styles.chatrow} justify="start">
-        <Card className={styles.messagebox}>
-          <Row>
-            <Typography.Text className={styles.user}>User 3</Typography.Text>
-          </Row>
-          <Row>
-            <Typography.Text className={styles.message}>hi</Typography.Text>
-          </Row>
-        </Card>
-      </Row>
-      <Row className={styles.chatrow} justify="start">
-        <Card className={styles.messagebox}>
-          <Row>
-            <Typography.Text className={styles.user}>User 2</Typography.Text>
-          </Row>
-          <Row>
-            <Typography.Text className={styles.message}>hi</Typography.Text>
-          </Row>
-        </Card>
-      </Row>
-      <Row className={styles.chatrow} justify="start">
-        <Card className={styles.messagebox}>
-          <Row>
-            <Typography.Text className={styles.user}>User 1</Typography.Text>
-          </Row>
-          <Row>
-            <Typography.Text className={styles.message}>hi</Typography.Text>
-          </Row>
-        </Card>
-      </Row>
-      <Row className={styles.chatrow} justify="end">
-        <Card className={`${styles.messagebox} ${styles.you}`} bordered>
-          <Row>
-            <Typography.Text className={styles.user}>You</Typography.Text>
-          </Row>
-          <Row>
-            <Typography.Text className={styles.message}>hello</Typography.Text>
-          </Row>
-        </Card>
-      </Row>
+        );
+      })}
       <Row ref={bottomRef} />
       <Row className={styles.textboxRow}>
         <Form.Item className={styles.textboxContainer} name="message">
